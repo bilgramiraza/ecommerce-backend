3cd48686ca39a21d1465eb28742c687f21900bb2
bilgramiraza <bilgramiraza@yahoo.com>
2022-12-17 22:13:40 +0530

    Added Basic Model Definations for Product Collection

--- models/product.js
+++ models/product.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const ProductSchema = new Schema({
+  name: { type: String, required: true, maxlength: 100 },
+  description: { type: String, required: true },
+  SKU: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+  quantity: { type: Number, required: true },
+  images: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+ProductSchema.virtual('url').get(() => {
+  return `/inventory/product/${this._id}`;
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
