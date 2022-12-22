3321312efa64978295e000d33c4154b00a567cfb
bilgramiraza <bilgramiraza@yahoo.com>
2022-12-22 13:29:52 +0530

    Created Route-Handlers for Category

---
+++ controllers/categoryController.js
@@ -0,0 +1,33 @@
+const Category = require('../models/category');
+
+exports.categoryList = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category List');
+};
+
+exports.categoryDetail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: Category Detail ${req.params.id}`);
+};
+
+exports.categoryCreateGet = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Create GET');
+};
+
+exports.categoryCreatePost = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Create POST');
+};
+
+exports.categoryDeleteGet = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Delete GET');
+};
+
+exports.categoryDeletePost = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Delete POST');
+};
+
+exports.categoryUpdateGet = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Update GET');
+};
+
+exports.categoryUpdatePost = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Update POST');
+};
