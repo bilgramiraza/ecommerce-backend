c8dab99c0b7e37bcae671c12a9513da1373fbbd8
bilgramiraza <bilgramiraza@yahoo.com>
2022-12-23 10:40:57 +0530

    Added Routes for Product and Category Pages And Updated Base Index Route

---
+++ routes/inventory.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+const categoryController = require('../controllers/categoryController');
+const productController = require('../controllers/productController');
+
+//PRODUCTS ROUTES //
+//Website HomePage
+router.get('/', productController.index);
+
+//Product Creation Page
+router.get('/product/create', productController.productCreateGet);
+router.post('/product/create', productController.productCreatePost);
+
+//Product Deletion Page
+router.get('/product/:id/delete', productController.productDeleteGet);
+router.post('/product/:id/delete', productController.productDeletePost);
+
+//Product Update Page
+router.get('/product/:id/update', productController.productUpdateGet);
+router.post('/product/:id/update', productController.productUpdatePost);
+
+//Product Details Page
+router.get('/product/:id', productController.productDetail);
+
+//List All Products
+router.get('/products', productController.productList);
+
+//CATEGORY ROUTES//
+//Category Creation Page
+router.get('/category/create', categoryController.categoryCreateGet);
+router.post('/category/create', categoryController.categoryCreatePost);
+
+//Category Deletion Page
+router.get('/category/:id/delete', categoryController.categoryDeleteGet);
+router.post('/category/:id/delete', categoryController.categoryDeletePost);
+
+//Category Update Page
+router.get('/category/:id/update', categoryController.categoryUpdateGet);
+router.post('/category/:id/update', categoryController.categoryUpdatePost);
+
+//Category Details Page
+router.get('/category/:id', categoryController.categoryDetail);
+
+//List All Categories
+router.get('/categories', categoryController.categoryList);
+
+module.exports = router;
