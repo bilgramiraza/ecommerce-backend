335ff0a0ac7b49c8fdcbfb7c77d5ddec31e3402a
bilgramiraza <bilgramiraza@yahoo.com>
2023-01-02 12:39:37 +0530

    Added Controller Function for Specific Category Details Route

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require('../models/category');
+const Product = require('../models/product');
 const async = require('async');
 
 // Export a function that handles the request to the '/products' route
@@ -9,8 +10,8 @@ exports.categoryList = (req, res, next) => {
     .sort({ name: 1 })
     .exec((err, listCategories) => {
       if (err) return next(err);
-      //To avoid an security expliot Handlebars cannot access properties of mongoose objects directly
-      //Hence, we are required to manually copy the properties to another variable
+      // To avoid a security exploit, Handlebars cannot access properties of Mongoose objects directly
+      // Hence, we are required to manually Specify the values that handlebars can access
       const copiedListCategories = listCategories.map((category) => {
         return {
           name: category.name,
@@ -25,7 +26,47 @@ exports.categoryList = (req, res, next) => {
     });
 };
 
-exports.categoryDetail = (req, res) => {};
+// Export a function that handles the request to the '/category/:id' route
+exports.categoryDetail = (req, res, next) => {
+  async.parallel(
+    {
+      category(callback) {
+        // Find the category with the specified ID
+        Category.findById(req.params.id).exec(callback);
+      },
+      categoryProducts(callback) {
+        // Find the products with the specified category ID
+        Product.find({ category: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) return next(err);
+      // If the category is not found, create an error object with a
+      //'Category not Found' message and a 404 status code
+      if (results.category == null) {
+        const err = new Error('Category not Found');
+        err.status = 404;
+        return next(err);
+      }
+      // To avoid a security exploit, Handlebars cannot access properties of Mongoose objects directly
+      // Hence, we are required to manually Specify the values that handlebars can access
+      const copiedCategoryProducts = results.categoryProducts.map((product) => {
+        return {
+          url: product.url,
+          name: product.name,
+          description: product.description,
+          quantity: product.quantity,
+          price: product.price,
+        };
+      });
+      res.render('categoryDetail', {
+        title: results.category.name,
+        categoryDescription: results.category.description,
+        categoryProducts: copiedCategoryProducts,
+      });
+    }
+  );
+};
 
 exports.categoryCreateGet = (req, res) => {
   res.send('NOT IMPLEMENTED: Category Create GET');
